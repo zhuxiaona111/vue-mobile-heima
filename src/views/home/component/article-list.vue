2ebde3dcae7ab783c133e9ca09b4d7b594d2e23c
zhuxiaona <zhuxiaona@example.com>
2020-01-15 20:00:46 +0800

    完成上拉加载，下拉刷新的模拟数据实现

--- src/views/home/component/article-list.vue
+++ src/views/home/component/article-list.vue
@@ -1,9 +1,14 @@
 <template>
+
   <div class="scroll-wrapper">
-    <van-list v-model="upLoading" :finished="finished" finished-text="没有了" @load="onLoad">
-      <!-- 渲染数据循环环遍历 -->
-      <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
-    </van-list>
+    <!-- 下拉刷新组件 -->
+    <van-pull-refresh v-model="downLoading" @refresh="onRefresh">
+      <!-- 上拉加载组件 -->
+      <van-list v-model="upLoading" :finished="finished" finished-text="没有了" @load="onLoad">
+        <!-- 渲染数据循环环遍历 -->
+        <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -12,26 +17,40 @@ export default {
   name: 'article-list',
   data () {
     return {
-      upLoading: false,
+      upLoading: false, // 开启刷拉加载状态
       finished: false,
-      articles: []
+      articles: [],
+      downLoading: false // 是否开启下拉刷新状态
     }
   },
   methods: {
     onLoad () {
+      console.log('获取数据')
       setTimeout(() => {
-        if (this.articles.length === 50) {
-          this.finished = true
+        // 模拟生成数组
+        if (this.articles.length < 50) {
+          let arr = Array.from(Array(10), (value, index) => this.articles.length + index + 1)
+          // 在数组后面追加数组，...arr解构数组
+          this.articles.push(...arr)
+          this.upLoading = false
         } else {
-          let arr = Array.from(Array(10), (value, index) => index + this.articles.length + 1)
-          this.articles.push(...arr) // 把生成的数据追加到末尾
-          this.upLoading = false // 关闭状态
+          this.finished = true// 加载完毕，改状态
         }
-      }, 2000)
-      console.log('开始加载数据')
+      }, 1000)
+    },
+    // 下拉刷新方法
+    onRefresh () {
+      console.log('下拉刷新')
+      setTimeout(() => {
+        let arr = Array.from(Array(10), (value, index) => ('追加' + (index + 1)))
+        this.articles.unshift(...arr)
+        this.downLoading = false
+      }, 1000)
     }
+
   }
 }
+
 </script>
 
 <style lang="less" scoped>
