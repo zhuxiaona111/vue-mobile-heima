8a19f878f1351ea60750311ce6c217c1635bf24a
zhuxiaona <zhuxiaona@example.com>
2020-01-21 13:24:33 +0800

    时间格式处理，图片懒加载

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Vant from 'vant'
+import Vant, { Lazyload } from 'vant'
 import 'vant/lib/index.less'
 import App from './App.vue'
 import router from './permission/router'
@@ -7,9 +7,11 @@ import store from './store'
 import 'amfe-flexible'
 import '@/styles/index.less'
 import plug from '@/utils/plug'
+
 Vue.config.productionTip = false
 Vue.use(Vant)
 Vue.use(plug)
+Vue.use(Lazyload)
 new Vue({
   router,
   store,
--- src/utils/plug.js
+++ src/utils/plug.js
@@ -1,12 +1,22 @@
+// 引入时间转换的插件
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn'
+dayjs.extend(relativeTime)
+export default {
+  install (Vue) {
+    Vue.prototype.$sleep = sleep
+    // 时间处理要用过滤器
+    Vue.filter('reltime', reltime)
+  }
+}
 // 封装休眠函数
 function sleep (time = 500) {
   return new Promise(function (resolve) {
     setTimeout(() => resolve(), time)
   })
 }
-
-export default {
-  install (Vue) {
-    Vue.prototype.$sleep = sleep
-  }
+// 定义时间处理函数
+function reltime (value) {
+  return dayjs().locale('zh-cn').from(value)
 }
